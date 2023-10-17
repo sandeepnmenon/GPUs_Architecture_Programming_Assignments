1c9fbadb28a99ab3016e52d91ae0ff799bea3657
Sandeep Menon <menonsandu@gmail.com>
2023-10-17 01:25:48 -0400

    matrix vector multiplication cuda

--- lab1/matrixvector.cu
+++ lab1/matrixvector.cu
@@ -2,13 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <cuda.h>
+#include <assert.h>
+#define assertm(exp, msg) assert(((void)msg, exp))
 
-int *matrixVectorMul_cpu(int *M, int *V, int m);
-int *matrixVectorMul_gpu(int *M, int *V, int m);
+int num_blocks, num_threads;
 
-int *matrixVectorMul_cpu(int *M, int *V, int m)
+__global__ void matrixVectorMulKernel(int *M_d, int *V_d, int *R_d, int m);
+void matrixVectorMul_cpu(int *M, int *V, int *R, int m);
+void matrixVectorMul_gpu(int *M, int *V, int *R, int m);
+
+void matrixVectorMul_cpu(int *M, int *V, int *R, int m)
 {
-    int *R = new int[m];
     for (int i = 0; i < m; i++)
     {
         R[i] = 0;
@@ -19,8 +24,27 @@ int *matrixVectorMul_cpu(int *M, int *V, int m)
         }
         R[i] = sum_row;
     }
+}
 
-    return R;
+__global__ void matrixVectorMulKernel(int *M_d, int *V_d, int *R_d, int m)
+{
+    int threadId = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = threadId; i < m; i += stride)
+    {
+        int sum_row = 0;
+        for (int j = 0; j < m; j++)
+        {
+            sum_row += M_d[i * m + j] * V_d[j];
+        }
+        R_d[i] = sum_row;
+    }
+}
+
+void matrixVectorMul_gpu(int *M, int *V, int *R, int m)
+{
+    matrixVectorMulKernel<<<num_blocks, num_threads>>>(M, V, R, m);
+    cudaDeviceSynchronize();
 }
 
 void fill_random(int *M, int *V, int m)
@@ -35,19 +59,58 @@ void fill_random(int *M, int *V, int m)
     }
 }
 
-double measure_time_func(int *(*func)(int *, int *, int), int *M, int *V, int m)
+bool check_result(int *R_cpu, int *R_gpu, int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        if (R_cpu[i] != R_gpu[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+double measure_time_func_cpu(void (*func)(int *, int *, int *, int), int *M, int *V, int *R, int m)
 {
     clock_t start, end;
     start = clock();
-    int *R = func(M, V, m);
+    func(M, V, R, m);
     end = clock();
-    delete[] R;
+    return ((double)(end - start)) / CLOCKS_PER_SEC;
+}
+
+double measure_time_func_gpu(void (*func_gpu)(int *, int *, int *, int), int *M, int *V, int *R, int *M_device, int *V_device, int *R_device, size_t size_M, size_t size_V, int m)
+{
+    clock_t start, end;
+    cudaError_t err;
+
+    // Start timing
+    start = clock();
+
+    // Copy data to device
+    err = cudaMemcpy(M_device, M, size_M, cudaMemcpyHostToDevice);
+    assertm(err == cudaSuccess, "Failed to copy matrix M from host to device");
+
+    err = cudaMemcpy(V_device, V, size_V, cudaMemcpyHostToDevice);
+    assertm(err == cudaSuccess, "Failed to copy vector V from host to device");
+
+    // Execute the kernel function
+    func_gpu(M_device, V_device, R_device, m);
+    cudaDeviceSynchronize();
+
+    // Copy result back to host
+    err = cudaMemcpy(R, R_device, size_V, cudaMemcpyDeviceToHost);
+    assertm(err == cudaSuccess, "Failed to copy vector R from device to host");
+
+    // End timing
+    end = clock();
+
     return ((double)(end - start)) / CLOCKS_PER_SEC;
 }
 
 int main(int argc, char **argv)
 {
-    int num_blocks, num_threads;
     if (argc != 3)
     {
         printf("Usage: %s <num_blocks> <num_threads>\n", argv[0]);
@@ -62,28 +125,56 @@ int main(int argc, char **argv)
 
     for (int dimension : test_dimensions)
     {
-        int *M = new int[dimension * dimension];
-        if (!M)
-        {
-            perror("Memory allocation failed for M");
-            exit(EXIT_FAILURE);
-        }
-        int *V = new int[dimension];
-        if (!V)
-        {
-            perror("Memory allocation failed for V");
-            exit(EXIT_FAILURE);
-        }
+        size_t size_M = dimension * dimension * sizeof(int);
+        int *M = (int *)malloc(size_M);
+        assertm(M, "Memory allocation failed for M");
+
+        size_t size_V = dimension * sizeof(int);
+        int *V = (int *)malloc(size_V);
+        assertm(V, "Memory allocation failed for V");
 
         srand(time(0));
         fill_random(M, V, dimension);
 
-        double cpu_time = measure_time_func(matrixVectorMul_cpu, M, V, dimension);
+        int *R = (int *)malloc(size_V);
+        assertm(R, "Memory allocation failed for R");
 
-        printf("Dimension: %d CPU time: %f\n", dimension, cpu_time);
+        double cpu_time = measure_time_func_cpu(matrixVectorMul_cpu, M, V, R, dimension);
 
-        delete[] M;
-        delete[] V;
+        printf("Dimension: %d\n", dimension);
+        printf("Sequential version: %f seconds\n", cpu_time);
+
+        int *M_device = nullptr;
+        int *V_device = nullptr;
+        int *R_device = nullptr;
+        cudaError_t err = cudaMalloc(&M_device, size_M);
+        assertm(err == cudaSuccess, "Failed to allocate device matrix M");
+
+        err = cudaMalloc(&V_device, dimension * sizeof(int));
+        assertm(err == cudaSuccess, "Failed to allocate device vector V");
+
+        err = cudaMalloc(&R_device, dimension * sizeof(int));
+        assertm(err == cudaSuccess, "Failed to allocate device vector R");
+
+        int *R_gpu = (int *)malloc(size_V);
+        assertm(R_gpu, "Memory allocation failed for R_gpu");
+
+        double gpu_time = measure_time_func_gpu(matrixVectorMul_gpu, M, V, R_gpu, M_device, V_device, R_device, size_M, size_V, dimension);
+        printf("GPU version: %f seconds\n", gpu_time);
+
+        err = cudaMemcpy(R_gpu, R_device, size_V, cudaMemcpyDeviceToHost);
+        assertm(err == cudaSuccess, "Failed to copy vector R from device to host");
+
+        assertm(check_result(R, R_gpu, dimension), "CPU and GPU results do not match");
+
+        printf("Speedup: %f\n", cpu_time / gpu_time);
+
+        free(M);
+        free(V);
+        free(R);
+        cudaFree(M_device);
+        cudaFree(V_device);
+        cudaFree(R_device);
     }
 
     return 0;
